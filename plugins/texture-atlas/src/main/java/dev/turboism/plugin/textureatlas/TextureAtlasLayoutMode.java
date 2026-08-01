15e73ab9f5f833d59f897f0f049ac39c468ee567
RainTrap341 <30723848+RainTrap341@users.noreply.github.com>
2026-08-01 17:20:39 +0000

    refactor(atlas-maxrects-bssf): rename layout plugin module/package from texture-atlas
    plugins/texture-atlas -> plugins/atlas-maxrects-bssf, package
    dev.turboism.plugin.textureatlas -> dev.turboism.plugin.atlasmaxrectsbssf;
    tests/build + manifest integration test paths updated.

--- plugins/texture-atlas/src/main/java/dev/turboism/plugin/textureatlas/TextureAtlasLayoutMode.java
+++
@@ -1,7 +0,0 @@
-package dev.turboism.plugin.textureatlas;
-
-/** Persisted plugin-owned automatic-layout policy. */
-public enum TextureAtlasLayoutMode {
-    PART_BUCKET,
-    COMPACT
-}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -19,7 +19,7 @@ include(
     "plugins:project-panel",
     "plugins:scene-palette-enhancer",
     "plugins:psd-import",
-    "plugins:texture-atlas",
+    "plugins:atlas-maxrects-bssf",
     "plugins:texture-atlas-stats",
     "plugins:physics-editor",
     "testframework",
